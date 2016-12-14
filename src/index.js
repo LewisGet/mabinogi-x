3326646151746b568769eaff1fdbbfb73d1d7d75
Kennardy Dewanto <kennardy.dewanto@mitrais.com>
2016-12-14 14:10:34 +0700

    [Chore] Add material-ui

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import App from './App';
 import './index.css';
 
+//
+// Setup 1 - react-tap-event-plugin
+//
+injectTapEventPlugin();
+
 ReactDOM.render(
-  <App />,
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
