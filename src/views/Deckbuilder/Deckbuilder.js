faec1d4249ece9695a9a8ae19a891fc57e4ef1e2
Kennardy Dewanto <kennardy.dewanto@mitrais.com>
2016-12-15 18:50:40 +0700

    [Feature] Implement <MabinogiCardList /> inside <Deckbuilder /> and wire it with cards data

--- src/views/Deckbuilder/Deckbuilder.js
+++ src/views/Deckbuilder/Deckbuilder.js
@@ -1,22 +1,53 @@
 import React, { Component } from 'react';
+import { fetchCards } from '../../actions';
 import config from '../../config';
 import Layout from '../../components/Layout';
+import MabinogiCardList from '../../components/MabinogiCardList';
+import { connect } from 'react-redux';
 import { Grid } from 'unsemantic';
 
-class Deckbuilder extends Component {
+export class Deckbuilder extends Component {
+  constructor(props) {
+    super(props);
+
+    this.props.fetchCards();
+  }
+
   render() {
     const { title } = config;
+    const { cards } = this.props;
+
     return (
       <Layout title={title}>
         <Grid desktop="20">
           Card Filter
         </Grid>
         <Grid desktop="80">
-          Card List
+          <MabinogiCardList
+            cards={cards}
+          />
         </Grid>
       </Layout>
     );
   }
 }
 
-export default Deckbuilder;
+const mapStateToProps = ({ cards }) => {
+  return {
+    cards, 
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchCards() {
+      dispatch(fetchCards());
+    },
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Deckbuilder);
+
--- src/views/Deckbuilder/Deckbuilder.test.js
+++ src/views/Deckbuilder/Deckbuilder.test.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import MabinogiCardList from '../../components/MabinogiCardList';
 import { shallow } from 'enzyme';
-import Deckbuilder from './Deckbuilder';
+import { Deckbuilder } from './Deckbuilder';
+
+/**
+ * Setup test data
+ */
+const cards = Array(100).fill(0).map((_, i) => ({
+  id: i.toString(),
+}));
+
+function setup() {
+  const props = {
+    fetchCards: jest.fn(),
+    cards,
+  };
+
+  return shallow(<Deckbuilder {...props} />);
+}
 
 describe('<Deckbuilder />', () => {
   it('should contain a Layout', () => {
-    const deckbuilder = shallow(<Deckbuilder />);
+    const deckbuilder = setup();
 
     expect(deckbuilder.find('Layout').length).toEqual(1);
+  });
+
+  it('should contain <MabinogiCardList /> with  cards passed in', () => {
+    const deckbuilder = setup();
+
+    expect(deckbuilder.find(MabinogiCardList).prop('cards')).toEqual(cards);
   });
 });
